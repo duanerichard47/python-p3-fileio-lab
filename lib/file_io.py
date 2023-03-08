6d3d6a74f67a0d3941c112ec7dad50292eea32a3
Duane <duanerichard47@hotmail.com>
2023-03-08 00:01:00 -0500

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,11 +1,11 @@
 def write_file(file_name, file_content):
-    with open(file_name, mode='w', encoding='utf-8') as log_file:
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as log_file:
         log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    with open(file_name, mode = 'a', encoding= 'utf-8') as log_file:
+    with open(f'{file_name}.txt', mode = 'a', encoding= 'utf-8') as log_file:
         log_file.write(append_content)
 
 def read_file(file_name):
-    with open(file_name, mode='r', encoding='utf-8'):
-        print(file_name)
+    with open(f'{file_name}.txt', mode='r', encoding='utf-8') as log_file:
+        return(log_file.read())
